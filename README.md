6af520c618c12ea419855b288a4b9ec657d1ca79
Yuri <yuri.karadzhov@gmail.com>
2020-05-08 14:14:14 +0300

    Allow absolute URLS and https

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Patch angular dynamic compiler to work without bundling
 ```sh
 npm install hqjs@babel-plugin-patch-angular-fesm5-compiler
 ```
+
+# Transformation
+Patch allows to resolve absolute URLs and URLs starting from `http` or `https`.
--- index.js
+++ index.js
@@ -5,18 +5,33 @@ module.exports = function({ types: t }) {
         if (!state.file.opts.filename.endsWith('compiler/fesm5/compiler.js')) return;
         const { node } = nodePath;
         if (node.id.name === 'isStyleUrlResolvable') {
+          const ifSt = node.body.body.find(child => t.isIfStatement(child));
+          if(t.isLogicalExpression(ifSt.test) && t.isReturnStatement(ifSt.consequent)) ifSt.test = ifSt.test.left;
+
           const ret = node.body.body.find(child => t.isReturnStatement(child));
           ret.argument = t.logicalExpression(
             '||',
             ret.argument,
-            t.binaryExpression(
-              '==',
-              t.memberExpression(
-                t.identifier('schemeMatch'),
-                t.identifier('1'),
-                true
+            t.logicalExpression(
+              '||',
+              t.binaryExpression(
+                '==',
+                t.memberExpression(
+                  t.identifier('schemeMatch'),
+                  t.identifier('1'),
+                  true
+                ),
+                t.stringLiteral('http')
               ),
-              t.stringLiteral('http')
+              t.binaryExpression(
+                '==',
+                t.memberExpression(
+                  t.identifier('schemeMatch'),
+                  t.identifier('1'),
+                  true
+                ),
+                t.stringLiteral('https')
+              )
             )
           );
         }
